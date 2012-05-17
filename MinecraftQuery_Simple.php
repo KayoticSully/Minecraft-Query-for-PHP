a8cf3709f7306c9e58831a751503521aaf79b417
Ivailo Stoyanov <ivko.mail@gmail.com>
2012-05-18 01:27:43 +0300

    switched to 4-space indentation, de-capitalized variables and functions

--- MinecraftQuery_Simple.php
+++ MinecraftQuery_Simple.php
@@ -1,41 +1,44 @@
 <?php
-	/*
-	 * Queries Minecraft server (1.8+)
-	 * Returns array on success, false on failure.
-	 *
-	 * Written by xPaw
-	 *
-	 * Website: http://xpaw.ru
-	 * GitHub: https://github.com/xPaw/PHP-Minecraft-Query
-	 */
-	
-	function QueryMinecraft( $IP, $Port = 25565, $Timeout = 2 )
-	{
-		$Socket = Socket_Create( AF_INET, SOCK_STREAM, SOL_TCP );
-		
-		Socket_Set_Option( $Socket, SOL_SOCKET, SO_SNDTIMEO, array( 'sec' => (int)$Timeout, 'usec' => 0 ) );
-		
-		if( $Socket === FALSE || @Socket_Connect( $Socket, $IP, (int)$Port ) === FALSE )
-		{
-			return FALSE;
-		}
-		
-		Socket_Send( $Socket, "\xFE", 1, 0 );
-		$Len = Socket_Recv( $Socket, $Data, 256, 0 );
-		Socket_Close( $Socket );
-		
-		if( $Len < 4 || $Data[ 0 ] != "\xFF" )
-		{
-			return FALSE;
-		}
-		
-		$Data = SubStr( $Data, 3 );
-		$Data = iconv( 'UTF-16BE', 'UTF-8', $Data );
-		$Data = Explode( "\xA7", $Data );
-		
-		return Array(
-			'HostName'   => SubStr( $Data[ 0 ], 0, -1 ),
-			'Players'    => isset( $Data[ 1 ] ) ? IntVal( $Data[ 1 ] ) : 0,
-			'MaxPlayers' => isset( $Data[ 2 ] ) ? IntVal( $Data[ 2 ] ) : 0
-		);
-	}
+/*
+ * Queries Minecraft server (1.8+)
+ * Returns array on success, false on failure.
+ *
+ * Originally written by xPaw
+ * Modifications and additions by ivkos
+ *
+ * GitHub: https://github.com/ivkos/Minecraft-Query-for-PHP
+ */
+
+function queryMinecraft($ip, $port = 25565, $timeout = 2)
+{
+    $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+    
+    socket_set_option($socket, SOL_SOCKET, SO_SNDTIMEO, array(
+        'sec' => (int) $timeout,
+        'usec' => 0
+    ));
+    
+    if ($socket === false || @socket_connect($socket, $ip, (int) $port) === false) {
+        return false;
+    }
+    
+    socket_send($socket, "\xFE", 1, 0);
+    $len = socket_recv($socket, $data, 256, 0);
+    socket_close($socket);
+    
+    if ($len < 4 || $data[0] != "\xFF") {
+        return false;
+    }
+    
+    $data = substr($Data, 3);
+    $data = iconv('UTF-16BE', 'UTF-8', $data);
+    $data = explode("\xA7", $data);
+    
+    return array(
+        'hostName'   => substr($data[0], 0, -1),
+        'players'    => isset($data[1]) ? intval($data[1]) : 0,
+        'maxPlayers' => isset($data[2]) ? intval($data[2]) : 0
+    );
+}
+
+?>
