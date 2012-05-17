e176cd8133c9ff08393b763719b3cd4b6807a1ce
Ivailo Stoyanov <ivko.mail@gmail.com>
2012-05-18 00:41:30 +0300

    switched to 4-space indentation; de-capitalized variables and some functions

--- MinecraftQuery.class.php
+++ MinecraftQuery.class.php
@@ -1,170 +1,145 @@
+
 <?php
-class MinecraftQueryException extends Exception
+class minecraftQueryException extends Exception
 {
-	// Exception thrown by MinecraftQuery class
 }
 
-class MinecraftQuery
+class minecraftQuery
 {
-	/*
-	 * Class written by xPaw
-	 *
-	 * Website: http://xpaw.ru
-	 * GitHub: https://github.com/xPaw/PHP-Minecraft-Query
-	 */
-	
-	const STATISTIC = 0x00;
-	const HANDSHAKE = 0x09;
-	
-	private $Socket;
-	private $Players;
-	private $Info;
-	
-	public function Connect( $Ip, $Port = 25565, $Timeout = 3 )
-	{
-		if( $this->Socket = FSockOpen( 'udp://' . $Ip, (int)$Port ) )
-		{
-			Socket_Set_TimeOut( $this->Socket, $Timeout );
-			
-			$Challenge = $this->GetChallenge( );
-			
-			if( $Challenge === false )
-			{
-				FClose( $this->Socket );
-				throw new MinecraftQueryException( "Failed to receive challenge." );
-			}
-			
-			if( !$this->GetStatus( $Challenge ) )
-			{
-				FClose( $this->Socket );
-				throw new MinecraftQueryException( "Failed to receive status." );
-			}
-			
-			FClose( $this->Socket );
-		}
-		else
-		{
-			throw new MinecraftQueryException( "Can't open connection." );
-		}
-	}
-	
-	public function GetInfo( )
-	{
-		return isset( $this->Info ) ? $this->Info : false;
-	}
-	
-	public function GetPlayers( )
-	{
-		return isset( $this->Players ) ? $this->Players : false;
-	}
-	
-	private function GetChallenge( )
-	{
-		$Data = $this->WriteData( self :: HANDSHAKE );
-		
-		return $Data ? Pack( 'N', $Data ) : false;
-	}
-	
-	private function GetStatus( $Challenge )
-	{
-		$Data = $this->WriteData( self :: STATISTIC, $Challenge . Pack( 'c*', 0x00, 0x00, 0x00, 0x00 ) );
-		
-		if( !$Data )
-		{
-			return false;
-		}
-		
-		$Last = "";
-		$Info = Array( );
-		
-		$Data    = SubStr( $Data, 11 ); // splitnum + 2 int
-		$Data    = Explode( "\x00\x00\x01player_\x00\x00", $Data );
-		$Players = SubStr( $Data[ 1 ], 0, -2 );
-		$Data    = Explode( "\x00", $Data[ 0 ] );
-		
-		// Array with known keys in order to validate the result
-		// It can happen that server sends custom strings containing bad things (who can know!)
-		$Keys = Array(
-			'hostname'   => 'HostName',
-			'gametype'   => 'GameType',
-			'version'    => 'Version',
-			'plugins'    => 'Plugins',
-			'map'        => 'Map',
-			'numplayers' => 'Players',
-			'maxplayers' => 'MaxPlayers',
-			'hostport'   => 'HostPort',
-			'hostip'     => 'HostIp'
-		);
-		
-		foreach( $Data as $Key => $Value )
-		{
-			if( ~$Key & 1 )
-			{
-				if( !Array_Key_Exists( $Value, $Keys ) )
-				{
-					$Last = false;
-					continue;
-				}
-				
-				$Last = $Keys[ $Value ];
-				$Info[ $Last ] = "";
-			}
-			else if( $Last != false )
-			{
-				$Info[ $Last ] = $Value;
-			}
-		}
-		
-		// Ints
-		$Info[ 'Players' ]    = IntVal( $Info[ 'Players' ] );
-		$Info[ 'MaxPlayers' ] = IntVal( $Info[ 'MaxPlayers' ] );
-		$Info[ 'HostPort' ]   = IntVal( $Info[ 'HostPort' ] );
-		
-		// Parse "plugins", if any
-		if( $Info[ 'Plugins' ] )
-		{
-			$Data = Explode( ": ", $Info[ 'Plugins' ], 2 );
-			
-			$Info[ 'RawPlugins' ] = $Info[ 'Plugins' ];
-			$Info[ 'Software' ]   = $Data[ 0 ];
-			
-			if( Count( $Data ) == 2 )
-			{
-				$Info[ 'Plugins' ] = Explode( "; ", $Data[ 1 ] );
-			}
-		}
-		else
-		{
-			$Info[ 'Software' ] = 'Vanilla';
-		}
-		
-		$this->Info = $Info;
-		
-		if( $Players )
-		{
-			$this->Players = Explode( "\x00", $Players );
-		}
-		
-		return true;
-	}
-	
-	private function WriteData( $Command, $Append = "" )
-	{
-		$Command = Pack( 'c*', 0xFE, 0xFD, $Command, 0x01, 0x02, 0x03, 0x04 ) . $Append;
-		$Length  = StrLen( $Command );
-		
-		if( $Length !== FWrite( $this->Socket, $Command, $Length ) )
-		{
-			return false;
-		}
-		
-		$Data = FRead( $this->Socket, 1440 );
-		
-		if( StrLen( $Data ) < 5 || $Data[ 0 ] != $Command[ 2 ] )
-		{
-			return false;
-		}
-		
-		return SubStr( $Data, 5 );
-	}
+    const STATISTIC = 0x00;
+    const HANDSHAKE = 0x09;
+    
+    private $socket;
+    private $players;
+    private $info;
+    
+    public function connect($ip, $port = 25565, $timeout = 3)
+    {
+        if ($this->socket = fsockopen('udp://' . $ip, (int) $port)) {
+            socket_set_timeout($this->socket, $timeout);
+            
+            $challenge = $this->getChallenge();
+            
+            if ($challenge === false) {
+                fclose($this->socket);
+                throw new minecraftQueryException("Failed to receive challenge.");
+            }
+            
+            if (!$this->getStatus($challenge)) {
+                fclose($this->socket);
+                throw new minecraftQueryException("Failed to receive status.");
+            }
+            
+            fclose($this->socket);
+        } else {
+            throw new minecraftQueryException("Can't open connection.");
+        }
+    }
+    
+    public function getInfo()
+    {
+        return isset($this->info) ? $this->info : false;
+    }
+    
+    public function getPlayers()
+    {
+        return isset($this->players) ? $this->players : false;
+    }
+    
+    private function getChallenge()
+    {
+        $data = $this->writeData(self::HANDSHAKE);
+        
+        return $data ? pack('N', $data) : false;
+    }
+    
+    private function getStatus($challenge)
+    {
+        $data = $this->writeData(self::STATISTIC, $challenge . pack('c*', 0x00, 0x00, 0x00, 0x00));
+        
+        if (!$data) {
+            return false;
+        }
+        
+        $last = "";
+        $info = array();
+        
+        $data    = substr($data, 11); // splitnum + 2 int
+        $data    = explode("\x00\x00\x01player_\x00\x00", $data);
+        $players = substr($data[1], 0, -2);
+        $data    = explode("\x00", $data[0]);
+        
+        // Array with known keys in order to validate the result
+        // It can happen that server sends custom strings containing bad things (who can know!)
+        $keys = array(
+            'hostname'   => 'hostName',
+            'gametype'   => 'gameType',
+            'version'    => 'version',
+            'plugins'    => 'plugins',
+            'map'        => 'map',
+            'numplayers' => 'players',
+            'maxplayers' => 'maxPlayers',
+            'hostport'   => 'hostPort',
+            'hostip'     => 'hostIp'
+        );
+        
+        foreach ($data as $key => $value) {
+            if (~$key & 1) {
+                if (!array_key_exists($value, $keys)) {
+                    $last = false;
+                    continue;
+                }
+                
+                $last        = $keys[$value];
+                $info[$last] = "";
+            } else if ($last != false) {
+                $info[$last] = $value;
+            }
+        }
+        
+        // Ints
+        $info['players']    = intval($info['players']);
+        $info['maxPlayers'] = intval($info['maxPlayers']);
+        $info['hostPort']   = intval($info['hostPort']);
+        
+        // Parse "plugins", if any
+        if ($info['plugins']) {
+            $Data = explode(": ", $info['plugins'], 2);
+            
+            $info['rawPlugins'] = $info['plugins'];
+            $info['software']   = $data[0];
+            
+            if (count($data) == 2) {
+                $info['plugins'] = explode("; ", $data[1]);
+            }
+        } else {
+            $info['software'] = 'Vanilla';
+        }
+        
+        $this->info = $info;
+        
+        if ($players) {
+            $this->players = explode("\x00", $players);
+        }
+        
+        return true;
+    }
+    
+    private function writeData($command, $append = "")
+    {
+        $command = pack('c*', 0xFE, 0xFD, $command, 0x01, 0x02, 0x03, 0x04) . $append;
+        $length  = strlen($command);
+        
+        if ($length !== fwrite($this->socket, $command, $length)) {
+            return false;
+        }
+        
+        $data = fread($this->socket, 1440);
+        
+        if (strlen($data) < 5 || $data[0] != $command[2]) {
+            return false;
+        }
+        
+        return substr($data, 5);
+    }
 }
